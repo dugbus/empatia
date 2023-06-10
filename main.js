215ce46383e5023becf55ab92a5cba119fa1796b
David Brown <2047439+dugbus@users.noreply.github.com>
2023-06-10 16:41:57 +0100

    adding contact form

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ import './connect.svg';
 import './contact-button.svg';
 import './favicon-32x32.png';
 import './arrow.svg';
+import './whats-app-icon.svg';
 
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear().toString();
 
 const contactToggle = document.querySelector(".contact-toggle");
+const contactPanel = document.querySelector(".contact-panel");
+contactToggle.addEventListener("click", () => {
+  contactPanel.classList.toggle('is-open');
+})
