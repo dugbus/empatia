557aaaf8a4d7952ad2dd74ad412ec4c8cd4b99c5
David Brown <2047439+dugbus@users.noreply.github.com>
2023-06-10 14:55:27 +0100

    reorganised images and optimised styles

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import './styles.css';
+import './empatia-logo.svg';
+import './connect.svg';
+import './contact-button.svg';
+import './favicon-32x32.png';
+import './arrow.svg';
 
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear().toString();
+
+const contactToggle = document.querySelector(".contact-toggle");
